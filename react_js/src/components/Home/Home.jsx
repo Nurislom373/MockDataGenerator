bcbfc2fd2f8722a87334444a8e6b2dd765aac9f0
nurislsom <nurislomx72@gmaial.com>
2022-11-18 21:42:41 +0500

    form js add

--- react_js/src/components/Home/Home.jsx
+++ react_js/src/components/Home/Home.jsx
@@ -1,9 +1,25 @@
 import './static/Home.css';
+import {useState} from "react";
 import {Link, NavLink, useNavigate} from "react-router-dom";
 import {ReactComponent as Logo} from "./static/undraw_online_cv_re_gn0a.svg";
 import {ReactComponent as X} from "./static/x.svg";
 
 function Home() {
+
+    const [inputList, setInputList] = useState([{fieldName: '', fieldType: ''}, {fieldName: '', fieldType: ''}]);
+
+    const handleInputChange = (e, index) => {
+        const {name, value} = e.target;
+        const list = [...inputList];
+        list[index][name] = value;
+        setInputList(list);
+
+    }
+
+    const handleAddClick = () => {
+        setInputList([...inputList, {fieldName: '', fieldType: ''}]);
+    }
+
     return (
         <div>
             <div className="container mt-5">
@@ -19,25 +35,34 @@ function Home() {
                                         <h5>Type</h5>
                                     </div>
                                 </div>
-                                <div className="row">
-                                    <div className="col-4">
-                                        <input type="text" className="form-control"/>
-                                    </div>
-                                    <div className="col-4">
-                                        <select className="form-select">
-                                            <option selected>Choose type</option>
-                                            <option value="ID">ID</option>
-                                            <option value="UUID">UUID</option>
-                                            <option value="NAME">NAME</option>
-                                        </select>
-                                    </div>
-                                    <div className="col-4">
-                                        <button className="btn btn-outline-dark">
-                                            <X width="100%"/>
-                                        </button>
-                                    </div>
-                                </div>
-                                <button className="btn btn-dark mt-3">Add Field</button>
+                                {
+                                    inputList.map((elem, i) => {
+                                        return (
+                                            <div key={i} className="row mb-3">
+                                                <div className="col-4">
+                                                    <input type="text" name="fieldName" id="fieldName"
+                                                           className="form-control"
+                                                           onChange={e => handleInputChange(e, i)}/>
+                                                </div>
+                                                <div className="col-4">
+                                                    <select name="fieldType" id="fieldType" className="form-select"
+                                                            onChange={e => handleInputChange(e, i)}>
+                                                        <option selected value="">Choose type</option>
+                                                        <option value="ID">ID</option>
+                                                        <option value="UUID">UUID</option>
+                                                        <option value="NAME">NAME</option>
+                                                    </select>
+                                                </div>
+                                                <div className="col-4">
+                                                    <button className="btn btn-outline-dark">
+                                                        <X width="100%"/>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        );
+                                    })
+                                }
+                                <button className="btn btn-dark mt-3" onClick={handleAddClick}>Add Field</button>
                             </div>
 
                             <div className="mb-3">
